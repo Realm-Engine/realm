3450463d8a1a4385e72de41e1dc3e0eccd88fe85
Nathan Lapp <32725020+MashedTatos@users.noreply.github.com>
2022-01-12 01:06:30 -0500

    Configuration for building to static library, experiment with hotreload

--- Projects/RealmGame/src/main.d
+++ Projects/RealmGame/src/main.d
@@ -1,17 +1,58 @@
 
-
-import std.stdio;
-import glfw3.api;
-import realm.game;
-int main()
+version(RealmDynamicLibrary)
 {
-
-	RealmGame game = new RealmGame(1280,720,"Realm!");
-
-	game.run();
-	scope(exit)
+	import core.sys.windows.windows;
+	import core.sys.windows.dll;
+	import core.runtime;
+	import realm.game;
+	import std.stdio;
+	import realm.engine.logging;
+	mixin SimpleDllMain;
+	pragma(msg,"Building project to dynamic library");
+	
+	void realm_main()
 	{
-		game.destroy();
+		writeln("rti start");
+		RealmGame game = new RealmGame(1280,720,"Realm!");
+
+		game.run();
+		scope(exit)
+		{
+			game.destroy();
+		}
 	}
-	return 1;
+}
+version(RealmExecutable)
+{
+	pragma(msg,"Building project to executable");
+	import realm.game;
+
+	int main()
+	{
+
+		RealmGame game = new RealmGame(1280,720,"Realm!");
+
+		game.run();
+		scope(exit)
+		{
+			game.destroy();
+		}
+		return 1;
+	}
+}
+
+version(RealmHotReload)
+{
+	import std.stdio;
+
+	import core.sys.windows.windows;
+	import core.runtime;
+	void main()
+	{
+		HMODULE h = cast(HMODULE)Runtime.loadLibrary("realmgame.dll");
+		FARPROC fp = GetProcAddress(h,"realm_main");
+		auto fun = cast(void function()) fp;
+		
+		fun();
+	}
 }
