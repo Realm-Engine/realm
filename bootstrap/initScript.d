c2945852995486a243537d26bf437340dec36083
Nathan Lapp <32725020+MashedTatos@users.noreply.github.com>
2022-05-25 19:46:45 -0400

    Download freetype and copy to bin

--- bootstrap/initScript.d
+++ bootstrap/initScript.d
@@ -8,33 +8,68 @@ import std.net.curl;
 import std.file;
 import std.json;
 import std.array;
-
+import std.zip;
+import std.string;
+import std.algorithm.iteration;
 void getFreetype(string [] args)
 {
-    if(!exists("../external/freetype-release"))
+
+	string root;
+	auto helpInformation = getopt(args,"realm-root", &root);
+	if(exists(root ~ "/external/freetype"))
 	{
-		
-		string freetypeVersion;
-		auto helpInformation = getopt(args,"ft-version",&freetypeVersion);
-		writeln("Freetype release not found, downloading version ", freetypeVersion);
-		auto http = HTTP();
-
-		http.addRequestHeader("Accept","application/vnd.github.v3+json");
-		string request = "https://api.github.com/repos/ubawurinna/freetype-windows-binaries/releases/tags/v" ~ freetypeVersion;
-
-		JSONValue response = parseJSON(get(request,http),JSONOptions.doNotEscapeSlashes);
-		writeln(response["zipball_url"].str());
-		JSONValue zipUrl = response["zipball_url"];
-
-		if(!zipUrl.isNull)
-		{
-			download(zipUrl.str(),"./freetype-release.zip");
-		}
-		else
-		{
-			writeln("Could not download freetype");
-		}
+		writeln("Freetype found, not downloading");
+		return;
 	}
+	mkdir(root ~ "/external/freetype");
+	
+	writeln("Freetype release not found, downloading ");
+	auto http = HTTP();
+
+	http.addRequestHeader("Accept","application/vnd.github.v3+json");
+	string request = "https://api.github.com/repos/realm-engine/freetype-windows-binaries/releases/latest";
+
+	JSONValue response = parseJSON(get(request,http),JSONOptions.doNotEscapeSlashes);
+	
+	JSONValue zipUrl = response["zipball_url"];
+
+	if(!zipUrl.isNull)
+	{
+
+		//download(zipUrl.str(),"./freetype.zip");
+		ubyte[] zipFile = get!(AutoProtocol, ubyte)(zipUrl.str);
+		auto zip = new ZipArchive(zipFile);
+		ArchiveMember[] directories;
+		ArchiveMember[] files;
+		foreach(ArchiveMember am; zip.directory)
+		{
+			if(am.expandedSize == 0)
+			{
+				directories ~= am;
+			}
+			else
+			{
+				files ~= am;
+			}
+		}
+		foreach(am; directories ~ files)
+		{
+			string outName = root ~ "/external/freetype/"~am.name[am.name.indexOf('/') + 1..am.name.length];
+			if(am.expandedSize == 0)
+			{
+				mkdirRecurse(outName);
+			}
+			else
+			{
+				zip.expand(am);
+				auto data = am.expandedData;
+				std.file.write(outName,data);
+			}
+
+		}
+
+	}
+   
 
     
 
